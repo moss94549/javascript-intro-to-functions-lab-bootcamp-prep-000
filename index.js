2caea04661ebc83486d0dea0c69b1cc1fd8620a9
moss94549@gmail.com <moss94549@gmail.com>
2018-04-07 04:48:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function sayHiToGrandma(sting)
     return ("I can't hear you!");
   else if (string.toUpperCase() === string)
     return ("YES INDEED!");
-  else if (string.valueOf)
+  else if (string.valueOf === )
   
 }
