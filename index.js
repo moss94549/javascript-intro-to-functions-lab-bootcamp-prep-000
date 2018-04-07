ee8210ac4ae417557d5933566a9148d336a3d1d3
moss94549@gmail.com <moss94549@gmail.com>
2018-04-07 05:06:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,5 +25,5 @@ function sayHiToGrandma(string)
   else if (string.toUpperCase() === string)
     return ("YES INDEED!");
   else if (string.valueOf() === "I love you, Grandma." )
-    return "I love you too.";
+    return "I love you, too.";
 }
