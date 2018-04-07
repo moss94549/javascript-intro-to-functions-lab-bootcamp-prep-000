ca3674a61da4e04c9bfdf996ed7cf08f457bcf51
moss94549@gmail.com <moss94549@gmail.com>
2018-04-07 05:06:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ function whisper(string)
 
 function logShout(string)
 {
-  console.log(string.toUpperCase);
+  console.log(string.toUpperCase());
 }
 
 function logWhisper(string)
 {
-  console.log(string.toLowerCase);
+  console.log(string.toLowerCase());
 }
 
 function sayHiToGrandma(string)
