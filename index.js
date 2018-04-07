f75ac855e8fb05079666cd447019080e73052fc0
moss94549@gmail.com <moss94549@gmail.com>
2018-04-07 04:59:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function whisper(string)
   return string.toLowerCase();
 }
 
-function sayHiToGrandma(sting)
+function sayHiToGrandma(string)
 {
   if (string.toLowerCase() === string)
     return ("I can't hear you!");
